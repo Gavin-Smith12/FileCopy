7a6882c7ca303ddb2efbff1fc7c45c9c06ba2792
git <ravi.serota@protonmail.com>
2019-10-13 19:43:37 -0400

    Changed packet type to char literals

--- fcpacket.h
+++ fcpacket.h
@@ -4,17 +4,17 @@
 #define MAX_DATA_SIZE 425
 
 struct initialPacket {
-	char packet_type = 8;                 // 1
+	char packet_type = '8';                 // 1
 	char checksum[SHA_DIGEST_LENGTH * 2]; // 40
     int numPackets;			              // 4
 	char filename[MAX_FILE_NAME];
 };
 
 struct dataPacket {
-	char packet_type = 9;					  // 1 bytes
+	char packet_type = '9';					  // 1 bytes
 	char checksum[SHA_DIGEST_LENGTH * 2];     // 40 bytes
     char fileNameHash[SHA_DIGEST_LENGTH * 2]; // 40 bytes
     int packetNum; 							  // 4 bytes
-    char data[MAX_DATA_SIZE]; /				  // Up to 425 bytes
+    char data[MAX_DATA_SIZE];    			  // Up to 425 bytes
     short dataSize;       					  // 2 bytes 
 };
